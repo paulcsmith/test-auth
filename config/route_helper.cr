b4bc7f44a2d897736875c08d65a1aac8c7af5fce
Paul Smith <paulcsmith0218@gmail.com>
2018-05-03 18:24:03 -0400

    Clarify

--- config/route_helper.cr
+++ config/route_helper.cr
@@ -4,7 +4,7 @@ Lucky::RouteHelper.configure do
     # Example: https://my_app.com
     settings.domain = ENV.fetch("APP_DOMAIN")
   else
-    # Set domain to the default host/port in development
+    # Set domain to the default host/port in development/test
     settings.domain = "http://#{Lucky::ServerSettings.host}:#{Lucky::ServerSettings.port}"
   end
 end
