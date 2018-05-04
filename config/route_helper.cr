baa09b70b911fe365cf4c537e1bbf38550a3d692
Paul Smith <paulcsmith@users.noreply.github.com>
2018-05-04 15:23:45 -0400

    Use new naming (#2)

--- config/route_helper.cr
+++ config/route_helper.cr
@@ -2,9 +2,9 @@
 Lucky::RouteHelper.configure do
   if Lucky::Env.production?
     # Example: https://my_app.com
-    settings.domain = ENV.fetch("APP_DOMAIN")
+    settings.base_uri = ENV.fetch("APP_DOMAIN")
   else
     # Set domain to the default host/port in development/test
-    settings.domain = "http://#{Lucky::ServerSettings.host}:#{Lucky::ServerSettings.port}"
+    settings.base_uri = "http://#{Lucky::ServerSettings.host}:#{Lucky::ServerSettings.port}"
   end
 end
