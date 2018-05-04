b4ba817d7d5e1564b3caf919a335be94c6c8462d
Paul Smith <paulcsmith@users.noreply.github.com>
2018-05-04 11:54:23 -0400

    Setup Travis to work with LuckyFlow and Postgres (#1)

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -8,7 +8,7 @@ require "../db/migrations/**"
 
 # Add/modify files in spec/setup to start/configure programs or run hooks
 #
-# By default there are scripts for setting up and cleaning the databse,
+# By default there are scripts for setting up and cleaning the database,
 # configuring LuckyFlow, starting the app server, etc.
 require "./setup/**"
 
